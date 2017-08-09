dabc659b7904d0c8e20a846b96dd2ad4372b7105
Tristan Burnside <tristan.burnside@outware.com.au>
2017-08-09 16:49:26 +1000

    Initial project structure and frameworks

---
+++ Card Dealer/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  Card Dealer
+//
+//  Created by Tristan Burnside on 9/8/17.
+//  Copyright © 2017 Tristan Burnside. All rights reserved.
+//
+
+import ReactiveSwift
+
+struct Card {
+  let suit: String
+  let number: String
+}
---
+++ Card DealerTests/CardSpec.swift
@@ -0,0 +1,24 @@
+//
+//  CardSpec.swift
+//  Card Dealer
+//
+//  Created by Tristan Burnside on 9/8/17.
+//  Copyright © 2017 Tristan Burnside. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import Card_Dealer
+
+class CardSpec: QuickSpec {
+  override func spec() {
+    describe("Tests of the card model") { 
+      context("a card is created") {
+        let card = Card(suit: "clubs", number: "9")
+        it("should have a suit", closure: { 
+          expect(card.suit).to(equal("clubs"))
+        })
+      }
+    }
+  }
+}
